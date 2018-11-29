99648396012d61d167cb9597517d2d8daa9f5ab7
Freddie Ruxton <freddie.ruxton@procensus.com>
2018-11-29 14:52:18 +0000

    Remove server

--- src/server/apis/cloudinary/contants.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  get              : 'get',
-  get_by_tag       : 'get_by_tag',
-  get_by_ids       : 'get_by_ids',
-  get_by_context   : 'get_by_context',
-  get_by_moderation: 'get_by_moderation',
-};
