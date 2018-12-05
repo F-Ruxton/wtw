35bcb90ab7815d698a49bcb4b3d5fa73c52c1c09
Freddie Ruxton <freddie.ruxton@procensus.com>
2018-12-05 19:39:13 +0000

    ttt

--- src/redux/reducers/UploadedPhotosReducer.js
+++ src/redux/reducers/UploadedPhotosReducer.js
@@ -1,7 +1,7 @@
 import {
     UPDATE_UPLOADED_PHOTO,
     DELETE_UPLOADED_PHOTO,
-} from '../../utils/images/constants';
+} from '../../utils/images/constants.js';
 
 const UploadedPhotosReducer = (uploadedPhotos = [], action) => {
     switch (action.type) {
