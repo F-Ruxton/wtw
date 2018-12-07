b74382be91c9f8d11bc7f8eaea81971c73d7d332
Freddie Ruxton <freddie.ruxton@procensus.com>
2018-12-07 15:37:34 +0000

    import compose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import PhotosListReducer from './redux/reducers/PhotosListReducer';
