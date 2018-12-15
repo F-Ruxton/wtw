f5d81108fcb2e29166300533cb79ca017c0c0b89
Freddie Ruxton <freddie.ruxton@procensus.com>
2018-12-15 11:32:45 +0000

    Log process

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -2,10 +2,17 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
 
+var api_key = function() {
+  console.log('process.env');
+  console.log(process.env);
+
+  return process.env.REACT_APP_CLOUDINARY_KEY;
+}
+
 module.exports = {
   cloud_name: 'wtw',
   upload_preset: 'UUUUUUUU',
-  api_key: process.env.REACT_APP_CLOUDINARY_KEY,
+  api_key: api_key(),
   api_secret: process.env.REACT_APP_CLOUDINARY_SECRET,
   secure: true,
 };
